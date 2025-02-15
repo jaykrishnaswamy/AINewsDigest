845c44bc1e37a03b37e832ca4e87dc3d991f9382
jaykrishnaswamy <38705328+jaykrishnaswamy@users.noreply.github.com>
2025-02-14 17:41:53 -0800

    Update AINews.py

--- .github/workflows/AINews.py
+++ .github/workflows/AINews.py
@@ -8,6 +8,8 @@ import os
 import requests
 import time
 from textblob import TextBlob
+import textwrap
+
 
 class Config:
     def __init__(self):
@@ -187,41 +189,16 @@ def format_telegram_message(digest_results):
 
     messages = []
     for source, content in digest_results.items():
-        message_parts = []
         message = f"🤖 <b>AI News Digest - {source}</b>\n\n"
+        message += f"<b>Executive Summary:</b>\n{content['executive_summary']}\n\n"
+        message += f"<b>Key Insights:</b>\n{content['key_insights']}\n\n"
+        message += f"<b>Product Innovation Opportunities:</b>\n{content['product_innovation']}\n\n"
+        message += f"<b>Key Concepts:</b>\n{content['key_concepts']}\n\n"
         
-        executive_summary = f"<b>Executive Summary:</b>\n{content['executive_summary']}\n\n"
-        key_insights = f"<b>Key Insights:</b>\n{content['key_insights']}\n\n"
-        product_innovation = f"<b>Product Innovation Opportunities:</b>\n{content['product_innovation']}\n\n"
-        key_concepts = f"<b>Key Concepts:</b>\n{content['key_concepts']}\n\n"
-        
-        if len(message + executive_summary) <= 4096:
-            message += executive_summary
-        else:
-            message_parts.append(message)
-            message = executive_summary
-        
-        if len(message + key_insights) <= 4096:
-            message += key_insights
-        else:
-            message_parts.append(message)
-            message = key_insights
-        
-        if len(message + product_innovation) <= 4096:
-            message += product_innovation
-        else:
-            message_parts.append(message)
-            message = product_innovation
-        
-        if len(message + key_concepts) <= 4096:
-            message += key_concepts
-        else:
-            message_parts.append(message)
-            message = key_concepts
-        
-        message_parts.append(message)
-        messages.extend(message_parts)
-        
+        # Split the message into chunks of a maximum of 4096 characters
+        message_chunks = textwrap.wrap(message, width=4096, break_long_words=False, replace_whitespace=False)
+        messages.extend(message_chunks)
+
     return messages
 
 def get_sentiment_label(sentiment):
